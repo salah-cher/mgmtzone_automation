503018dba0ea079d69cfa341487d2d85f564e0cb
salah-cher <32401751+salah-cher@users.noreply.github.com>
2019-02-14 14:21:12 -0500

    Update Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python
 
+'''
+    This script prints hostlists of our datacenter.
+    To be used (not only) by:
+    * Ansible as a dynamic inventory
+    * the checkConnectivity utility scripts
+
+    At some point in the future we might to merge this Inventory with the Monitoring Inventory:
+    https://enterprise-stash.hybris.com/projects/HCS-PDO-MON/repos/project-x/browse/inventory.py
+'''
+
+import argparse
+import os
+import re
+import json
+import socket
